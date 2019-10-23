3a8f7251a457b08e6f86eb49dc8cb82ba360cd0a
Paul Grubbs <pag225@cornell.edu>
2019-10-23 15:18:33 -0400

    hw3

---
+++ maul.py
@@ -0,0 +1,37 @@
+from requests import codes, Session
+
+LOGIN_FORM_URL = "http://localhost:8080/login"
+SETCOINS_FORM_URL = "http://localhost:8080/setcoins"
+
+def do_login_form(sess, username,password):
+	data_dict = {"username":username,\
+			"password":password,\
+			"login":"Login"
+			}
+	response = sess.post(LOGIN_FORM_URL,data_dict)
+	return response.status_code == codes.ok
+
+def do_setcoins_form(sess,uname, coins):
+	data_dict = {"username":uname,\
+			"amount":str(coins),\
+			}
+	response = sess.post(SETCOINS_FORM_URL, data_dict)
+	return response.status_code == codes.ok
+
+
+def do_attack():
+	sess = Session()
+  #you'll need to change this to a non-admin user, such as 'victim'.
+	uname =""
+	pw = ""
+	assert(do_login_form(sess, uname,pw))
+	#Maul the admin cookie in the 'sess' object here
+  
+	target_uname = uname
+	amount = 5000
+	result = do_setcoins_form(sess, target_uname,amount)
+	print("Attack successful? " + str(result))
+
+
+if __name__=='__main__':
+	do_attack()
---
+++ poattack.py
@@ -0,0 +1,57 @@
+import os
+from cryptography.hazmat.primitives import hashes, padding, ciphers
+from cryptography.hazmat.backends import default_backend
+
+import base64
+import binascii
+
+
+#You should implement this padding oracle object
+#to craft the requests containing the mauled
+#ciphertexts to the right URL.
+class PaddingOracle(object):
+
+    def __init__(self, po_url):
+        self.url = po_url
+        self._block_size_bytes = ciphers.algorithms.AES.block_size/8
+
+    @property
+    def block_length(self):
+        return self._block_size_bytes
+
+    #you'll need to send the provided ciphertext
+    #as the admin cookie, retrieve the request,
+    #and see whether there was a padding error or not.
+    def test_ciphertext(self, ct):
+        pass
+
+def split_into_blocks(msg, l):
+    while msg:
+        yield msg[:l]
+        msg = msg[l:]
+    
+def po_attack_2blocks(po, ctx):
+    """Given two blocks of cipher texts, it can recover the first block of
+    the message.
+    @po: an instance of padding oracle. 
+    @ctx: a ciphertext 
+    """
+    assert len(ctx) == 2*po.block_length, "This function only accepts 2 block "\
+        "cipher texts. Got {} block(s)!".format(len(ctx)/po.block_length)
+    c0, c1 = list(split_into_blocks(ctx, po.block_length))
+    msg = ''
+    # TODO: Implement padding oracle attack for 2 blocks of messages.
+    return msg
+
+def po_attack(po, ctx):
+    """
+    Padding oracle attack that can decrpyt any arbitrary length messags.
+    @po: an instance of padding oracle. 
+    You don't have to unpad the message.
+    """
+    ctx_blocks = list(split_into_blocks(ctx, po.block_length))
+    nblocks = len(ctx_blocks)
+    # TODO: Implement padding oracle attack for arbitrary length message.
+
+
+    
